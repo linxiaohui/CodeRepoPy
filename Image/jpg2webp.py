0bb6a47225b0638dec360dd441ed506f9295a4b1
linxiaohui <llinxiaohui@126.com>
2020-08-27 15:35:43 +0800

    JPG conver to webp

---
+++ Image/jpg2webp.py
@@ -0,0 +1,13 @@
+from PIL import Image
+def save_image(input_name, output_name):
+    im = Image.open(input_name)
+    if im.mode=="RGBA":
+        im.load()  # required for png.split()
+        background = Image.new("RGB", im.size, (255, 255, 255))
+        background.paste(im, mask=im.split()[3])  # 3 is the alpha channel
+        im = background
+    im.save('{}.webp'.format(output_name),'WEBP')
+
+save_image("a.jpg","x")
+
+
